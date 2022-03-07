bfe2cf3fa7397c888d410a8d3a27153ea38f8e6c
Xinglong Zhang <xinglong.zhang@chem.ox.ac.uk>
2022-03-07 15:35:05 +0800

    WIP: simplify plotter

---
+++ io/plotterstyles.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+
+def pretty_3d_plot(width=8, height=None, plt=None, dpi=None, color_cycle=("qualitative", "Set1_9")):
+    """
+    modified from pymatgen.utils.plotting for plotting 3D plot
+
+    Returns:
+        Matplotlib plot object with properly sized fonts.
+    """
+    ticksize = int(width * 2.5)
+
+    golden_ratio = (math.sqrt(5) - 1) / 2
+
+    if not height:
+        height = int(width * golden_ratio)
+
+    if plt is None:
+        import importlib
+
+        import matplotlib.pyplot as plt
+
+        mod = importlib.import_module("palettable.colorbrewer.%s" % color_cycle[0])
+        colors = getattr(mod, color_cycle[1]).mpl_colors
+        from cycler import cycler
+
+        plt.figure(figsize=(width, height), facecolor="w", dpi=dpi)
+        ax = plt.gca()
+        ax.set_prop_cycle(cycler("color", colors))
+    else:
+        fig = plt.gcf()
+        fig.set_size_inches(width, height)
+    plt.xticks(fontsize=ticksize)
+    plt.yticks(fontsize=ticksize)
+
+    ax = plt.gca()
+    ax.set_title(ax.get_title(), size=width * 4)
+
+    labelsize = int(width * 3)
+
+    ax.set_xlabel(ax.get_xlabel(), size=labelsize)
+    ax.set_ylabel(ax.get_ylabel(), size=labelsize)
+
+    return plt
+
--- plotters/barplots.py
+++ plotters/barplots.py
@@ -4,12 +4,24 @@ from pymatgen.util.plotting import pretty_plot, pretty_plot_two_axis, pretty_pol
 from pyplotter.io.parser import FileReader
 from pyatoms.utils.utils import lazy_property
 
-from pyatoms.utils.logging import create_logger
-logger = create_logger()
+import logging
+logger = logging.getLogger(__name__)
 
+from pyatoms.utils.logging import create_logger
+create_logger()
 
 class Plotter(object):
-    def __init__(self, filename):
+    def __init__(
+            self, filename, plot_mode='line',
+            x_col=0, y_col=1, z_col=None,  # using python 0-index throughout
+            cols_to_plot=None,  # specify which columns to plot
+            ylabel=None, zlabel=None,  # specify label for y- and z-axis
+            xmin=None, xmax=None, ymin=None, ymax=None, zmin=None, zmax=None, # specify ranges for plot
+            title=None,  # specify title for the plot
+            bar_width=0.4,   # default width for bar plots
+            plot_width=10,  # default plot width
+            plot_height=7   # default plot height
+    ):
         self.filename = filename
         self.parser = FileReader(filename=filename)
         self.basename = self.parser.basename
@@ -17,6 +29,124 @@ class Plotter(object):
         self.num_data = self.parser.num_data
         self.labels = self.parser.read_labels()
         self.data = self.parser.read_datapoints()
+        # plot defaults
+        self.mode = plot_mode
+        self.x_col = x_col
+        self.y_col = y_col
+        self.z_col = z_col
+        self.cols_to_plot = cols_to_plot
+        self.ylabel = ylabel
+        self.zlabel = zlabel
+
+        # default ranges from data; else return None
+        if x_col is not None:
+            self.data_xmin = min(self.data[x_col])
+            self.data_xmax = max(self.data[x_col])
+        if y_col is not None:
+            self.date_ymin = min(self.data[y_col])
+            self.data_ymax = max(self.data[y_col])
+        if z_col is not None:
+            self.data_zmin = min(self.data[z_col])
+            self.data_zmax = max(self.data[z_col])
+        self.bar_width = bar_width
+        self.plot_width = plot_width
+        self.plot_height = plot_height
+
+        plt = pretty_plot(width=plot_width, height=plot_height)
+        self.plt = plt
+
+    def plot_2d_line_scatter_bars(self):
+        assert plot_mode is not None, f'Plot mode is required!\n' \
+                                      f'Available plot modes are "scatter", "line", "bar", "grouped_bar".'
+
+        # get 2D data for plotting
+        x_data = self.data[x_col]
+        y_data = self.data[y_col]
+        assert len(x_data) == len(y_data), f'Lens of data for plotting: {x_data} and {y_data} are not the same!'
+        logger.info(f'x_data for plotting: {x_data}\n')
+        logger.info(f'y_data for plotting: {y_data}\n')
+
+        plt = self.plt
+
+        logger.info(f'Plotting in "{self.plot_mode}" mode.')
+
+        if plot_mode == 'line':
+            plt.plot(x_data, y_data, ls='-', lw=1.5)
+        elif plot_mode == 'scatter':
+            plt.scatter(x_data, y_data, marker='o', lw=1.5)
+        elif plot_mode == 'bar':
+            x_data = [int(i) for i in x_data]
+            index = np.arange(len(x_data))
+            plt.xticks(index, x_data)
+            plt.bar(index, y_data, width=width, color='green')
+        elif plot_mode == 'grouped_bar':
+            index = np.arange(1, len(x_data)+1) * self.num_cols
+            x_data_labels = [str(i) for i in x_data]
+            plt.xticks(index, x_data_labels)
+            x_data = np.array(x_data)
+            bar_width = self.bar_width + self.num_cols * 0.1
+            if self.cols_to_plot is not None:
+                plot_range_list = self.cols_to_plot
+                basename = basename + '_' + str(len(plot_range_list)) + '_cols'
+            else:
+                plot_range_list = range(1, self.num_cols)
+            for i in plot_range_list:
+                offset = i - self.num_cols//2
+                offset_width = offset * bar_width
+                logger.info(f'{self.data[i]}')
+                logger.info(f'{self.labels[i]}')
+                plt.bar(index + offset_width, self.data[i], width=bar_width, label=str(self.labels[i]))
+
+
+
+    def set_plot_2d(
+            self,
+            plt,  # plt object to be returned
+            ylabel=None, # specify label for y-axis
+            xmin=None, xmax=None, ymin=None, ymax=None, # specify ranges for plot
+            title=None # specify title for the plot
+    ):
+        # set plot limits
+        if xmin is not None and xmax is not None:
+            plt.xlim(xmin, xmax)
+        elif xmax is not None:
+            plt.xlim(self.data_xmax, xmax)
+        elif xmin is not None:
+            plt.xlim(xmin, self.data_xmax)
+
+        if ymin is not None and ymax is not None:
+            plt.ylim(ymin, ymax)
+        elif ymax is not None:
+            plt.ylim(self.data_ymin, ymax)
+        elif ymin is not None:
+            plt.ylim(ymin, self.data_ymax)
+
+        # set up legends
+        if self.labels is not None:
+            logger.info(f'labels: {self.labels}')
+            plt.xlabel(f'{self.labels[x_col].replace("_", " ")}', fontsize=24)
+            if ylabel is not None:
+                if '\\' in ylabel:
+                    ylabel_elem = ylabel.split('\\')
+                    ylabel = ylabel_elem[0] + f'$\{ylabel_elem[1]}$'
+                    plt.ylabel(fr'{ylabel}', fontsize=24)
+                else:
+                    plt.ylabel(f'{ylabel}', fontsize=24)
+            else:
+                plt.ylabel(f'{self.labels[y_col].replace("_", " ")}', fontsize=24)
+            plt.legend(loc='upper center', ncol=self.num_data)
+
+        # set up title
+        if title is not None:
+            plt.title(f'{title}')
+
+        # # save results
+        # plt.savefig(f'{basename}.pdf', bbox_inches='tight')
+        # plt.show()
+        # plt.clf()
+        return plt
+
+
 
     def plot_cols(
             self,
@@ -107,3 +237,11 @@ class Plotter(object):
         plt.savefig(f'{basename}.pdf', bbox_inches='tight')
         plt.show()
         plt.clf()
+
+    def plot_3d(
+            self, x_col, y_col, z_col  # 1-indexed column numbers for plotting
+    ):
+
+        pass
+
+
